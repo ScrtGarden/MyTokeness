b027c5cc3dc850116fa6b2e4f164b51610142a7d
hieulai <>
2021-03-23 13:33:58 +1100

    💄 add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from './styles'
+
+const Header = () => {
+  return <Container>Header</Container>
+}
+
+export default Header
---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  border: 1px solid;
+  font-size: ${(props) => props.theme.fontSizes.lg};
+  padding: 2rem;
+  text-align: center;
+`
+
+export { Container }
