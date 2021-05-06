07de937d758fbeaa8c088cac7f390f4278e22926
hieulai <>
2021-05-06 16:34:22 +1000

    💄 add button

---
+++ src/components/UI/Buttons/index.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+interface ButtonProps {
+  readonly size?: 'small' | 'medium' | 'large'
+  readonly isStretched?: boolean
+  readonly isPrimary?: boolean
+  readonly isSecondary?: boolean
+  readonly isDanger?: boolean
+}
+
+const Button = styled.button<ButtonProps>`
+  background-color: ${(props) => props.theme.buttons.button.bg.default.base};
+  border: none;
+  border-radius: 500px;
+  color: ${(props) => props.theme.font.colors.primary};
+  cursor: pointer;
+  font-size: ${(props) => props.theme.font.sizes.md};
+  font-weight: ${(props) => props.theme.font.weights.semibold};
+  height: 30px;
+  outline: none;
+  padding: 0 ${(props) => props.theme.space.sm};
+  transition: background-color 0.3s ease;
+  ${(props) => props.isStretched && 'width: 100%'};
+
+  :hover {
+    background-color: ${(props) => props.theme.buttons.button.bg.default.hover};
+  }
+
+  :active {
+    background-color: ${(props) =>
+      props.theme.buttons.button.bg.default.active};
+  }
+`
+
+export { Button }
