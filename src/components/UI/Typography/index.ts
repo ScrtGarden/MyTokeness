94c640b1f785c7a023e84be4f0cb07702323d46c
hieulai <>
2021-05-06 17:40:21 +1000

    💄 add typography

---
+++ src/components/UI/Typography/index.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+interface PageTitleProps {
+  readonly center?: boolean
+}
+
+const PageTitle = styled.h1<PageTitleProps>`
+  color: ${(props) => props.theme.font.colors.primary};
+  font-size: 5rem;
+  font-weight: ${(props) => props.theme.font.weights.semibold};
+  margin-bottom: ${(props) => props.theme.space.lg};
+  margin-top: 0;
+  ${(props) => props.center && 'text-align: center'};
+`
+
+const Text = styled.p`
+  color: ${(props) => props.theme.font.colors.secondary};
+  font-size: ${(props) => props.theme.font.sizes.md};
+  line-height: ${(props) => props.theme.font.lineHeights.md};
+  margin: 0;
+`
+
+export { PageTitle, Text }
