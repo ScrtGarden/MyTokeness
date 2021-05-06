a32ab6c84d93a67589eaf03ee7bbe859039073cf
hieulai <>
2021-05-06 16:34:08 +1000

    💄 add themes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,22 +1,78 @@
 const theme = {
-  light: {
-    fontSizes: {
-      "xs": "1rem",
-      "sm": "1.2rem",
-      "md": "1.4rem",
-      "lg": "1.8rem",
-      "xl": "2.2rem",
-      "xxl": "2.6rem",
-      "xxxl": "3.6rem"
-    },
-    "palette": {
-      "black": "#000",
-      "white": "#fff",
-    }
-  },
   dark: {
+    bg: '#121d25',
+    fg: '#1d2c38',
+    border: {
+      color: '#293c48',
+    },
+    brand: {
+      color: '',
+    },
+    font: {
+      sizes: {
+        xs: '1rem',
+        sm: '1.2rem',
+        md: '1.4rem',
+        lg: '1.8rem',
+        xl: '2.2rem',
+        xxl: '2.6rem',
+        xxxl: '3.6rem',
+      },
+      colors: {
+        brand: '#6b6af9',
+        primary: '#fff',
+        secondary: '#889aa7',
+        disabled: '#404a50',
+        error: '#de5f5f',
+        warn: '#de5f5f',
+      },
+      weights: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        regular: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+      lineHeights: {
+        sm: '1.6rem',
+        md: '2rem',
+        lg: '2.4rem',
+        xl: '2.8rem',
+        xxl: '3.2rem',
+        xxxl: '4.4rem',
+      },
+    },
+    space: {
+      base: 4,
+      xxs: '4px',
+      xs: '8px',
+      sm: '12px',
+      md: '20px',
+      lg: '32px',
+      xl: '40px',
+      xxl: '48px',
+    },
+
+    // buttons
+    buttons: {
+      button: {
+        bg: {
+          default: {
+            base: '#e3a92a',
+            hover: '#ca9624',
+            active: '#b18421',
+          },
+        },
+      },
+    },
+  },
+  light: {
     //
-  }
+  },
 }
 
 export default theme
