f7a461b37e3a6a366a0d0c929c0dd5edb309974e
hieulai <>
2021-05-11 16:14:12 +1000

    💄 add button with loading

---
+++ src/components/Common/ButtonWithLoading/index.tsx
@@ -0,0 +1,25 @@
+import { FC, memo } from 'react'
+
+import { ButtonProps } from '../../UI/Buttons'
+import { Button } from '../../UI/Buttons'
+import { StyledDots } from './styles'
+
+type OwnProps = {
+  text?: string
+  loading?: boolean
+  className?: string
+}
+
+type Props = ButtonProps & OwnProps
+
+const ButtonWithLoading: FC<Props> = (props) => {
+  const { loading, text = 'Add text', disabled, ...rest } = props
+
+  return (
+    <Button {...rest} disabled={loading || disabled}>
+      {loading ? <StyledDots /> : text}
+    </Button>
+  )
+}
+
+export default memo(ButtonWithLoading)
