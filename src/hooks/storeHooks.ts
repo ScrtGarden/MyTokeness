88a557e4b163035f1b62fe62995421220d828b25
hieulai <>
2021-05-11 16:36:06 +1000

    🚚 move file into hooks dir

---
+++ src/hooks/storeHooks.ts
@@ -0,0 +1,12 @@
+import { createTypedHooks } from 'easy-peasy'
+
+import { StoreModel, StoreState } from '../../store'
+
+const {
+  useStoreActions,
+  useStoreDispatch,
+  useStore,
+  useStoreState,
+} = createTypedHooks<StoreModel>()
+
+export { useStoreActions, useStoreState, useStoreDispatch, useStore }
