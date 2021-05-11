1c45e6af71bd4bd0658c9737fb2cf1f0669b6978
hieulai <>
2021-05-11 10:49:02 +1000

    ✨ add regex patterns

---
+++ utils/regexPatterns.ts
@@ -0,0 +1,11 @@
+// Only alphabetic chars and digits, up to 6 chars max.
+const symbolPattern = /^[A-Za-z0-9]{0,6}$/
+
+// Only integers, no leading zero
+const decimalsPattern = /^([1-9][0-9]*)$/
+
+// Only numbers, and amount of decimals is dynamic
+const amountPattern = (decimals: number | string) =>
+  `^\\d{1,}(\\.\\d{0,${decimals}})?$`
+
+export { symbolPattern, decimalsPattern, amountPattern }
