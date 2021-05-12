f6638ff46256af90f94a1d3c47de4e06a66df1ba
hieulai <>
2021-05-12 22:18:39 +1000

    🏷️ add result type

--- src/hooks/useMutationInitContract.ts
+++ src/hooks/useMutationInitContract.ts
@@ -1,9 +1,12 @@
-import { useMutation } from 'react-query'
+import { UseMutationResult, useMutation } from 'react-query'
+import { InstantiateResult } from 'secretjs'
 
-import { instantiateContract } from '../../utils/contractInteractions'
+import { Params, instantiateContract } from '../../utils/contractInteractions'
 
-const useMutationInitContract = () => {
-  return useMutation(instantiateContract)
-}
+const useMutationInitContract = (): UseMutationResult<
+  InstantiateResult,
+  Error,
+  Params
+> => useMutation(instantiateContract)
 
 export default useMutationInitContract
