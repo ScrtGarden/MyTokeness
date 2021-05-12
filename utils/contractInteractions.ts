a3bf89496e4d18d61ff097b95bb8457c99f2d590
hieulai <>
2021-05-12 13:43:55 +1000

    ✨ function that instantiate contract

---
+++ utils/contractInteractions.ts
@@ -0,0 +1,59 @@
+import { InstantiateResult } from 'secretjs'
+
+import { InitMsg as Snip20InitMsg } from '../interface/snip20'
+import keplr from './keplr'
+
+type Params = {
+  initMsg: Snip20InitMsg
+  label: string
+  codeId: number
+  maxGas: string
+}
+
+const instantiateContract = async (
+  data: Params
+): Promise<InstantiateResult> => {
+  const { initMsg, label, codeId, maxGas } = data
+
+  const signingClient = await keplr.createSigningClient({
+    maxGas,
+  })
+
+  try {
+    return await signingClient.instantiate(codeId, initMsg, label)
+  } catch (error) {
+    throw error
+  }
+}
+
+// export type ExecuteContractParams = {
+//   maxGas: string
+//   contractAddress: string
+//   handleMsg: HandleMsg
+// }
+
+// interface ExecuteContractResponse extends Partial<ExecuteResult> {
+//   error?: Error
+// }
+
+// /**
+//  * contract execution function
+//  */
+
+// const executeContract = async (
+//   params: ExecuteContractParams
+// ): Promise<ExecuteContractResponse> => {
+//   const { handleMsg, contractAddress, maxGas } = params
+//   const { secretjs } = await keplr.createSigningClient({
+//     maxGas,
+//   })
+
+//   try {
+//     const response = await secretjs?.execute(contractAddress, handleMsg)
+//     return { ...response }
+//   } catch (error) {
+//     throw error
+//   }
+// }
+
+export { instantiateContract }
