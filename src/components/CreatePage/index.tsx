6129c7835441e3dd74b9ef3ea4dafd1e9d54dafb
hieulai <>
2021-05-12 13:41:49 +1000

    ✨ add creation of snip20

--- src/components/CreatePage/index.tsx
+++ src/components/CreatePage/index.tsx
@@ -1,6 +1,13 @@
+import cryptoRandomString from 'crypto-random-string'
+import { format } from 'date-fns'
 import { MouseEvent } from 'react'
 
-import { Button } from '../UI/Buttons'
+import { CONTRACT_CODE_ID, MAX_GAS } from '../../../utils/constants'
+import { useStoreState } from '../../hooks/storeHooks'
+import useMutationConnectWallet from '../../hooks/useMutationConnectWallet'
+import useMutationGetAccounts from '../../hooks/useMutationGetAccounts'
+import useMutationInitContract from '../../hooks/useMutationInitContract'
+import ButtonWithLoading from '../Common/ButtonWithLoading'
 import { Container, InnerContainer } from '../UI/Containers'
 import { PageTitle } from '../UI/Typography'
 import Configuration from './Form/Configuration'
@@ -8,23 +15,80 @@ import Details from './Form/Details'
 import InitialBalances from './Form/InitialBalances'
 import Review from './Form/Review'
 import { Form } from './Form/styles'
+import { formatter } from './lib'
 import Store from './Store'
 import { Content } from './styles'
 
 const CreatePage = () => {
+  // store state
+  const isConnected = useStoreState((state) => state.auth.isWalletConnected)
+
   // context store state
+  const name = Store.useStoreState((state) => state.name)
+  const symbol = Store.useStoreState((state) => state.symbol)
+  const decimals = Store.useStoreState((state) => state.decimals)
+  const adminAddress = Store.useStoreState((state) => state.adminAddress)
+  const enablePublicTokenSupply = Store.useStoreState(
+    (state) => state.enablePublicTokenSupply
+  )
+  const enableDeposit = Store.useStoreState((state) => state.enableDeposit)
+  const enableRedeem = Store.useStoreState((state) => state.enableRedeem)
+  const enableMint = Store.useStoreState((state) => state.enableMint)
+  const enableBurn = Store.useStoreState((state) => state.enableBurn)
+  const initialBalances = Store.useStoreState((state) => state.initialBalances)
   const hasErrors = Store.useStoreState((state) => state.validation.hasErrors)
 
   // context store actions
   const setState = Store.useStoreActions((actions) => actions.setState)
 
-  const onClickCreate = (e: MouseEvent<HTMLButtonElement>) => {
+  // custom hooks
+  const {
+    mutateAsync: connectWallet,
+    isLoading: connecting,
+  } = useMutationConnectWallet()
+  const {
+    mutateAsync: getAccounts,
+    isLoading: gettingAccounts,
+  } = useMutationGetAccounts()
+  const { mutate, isLoading } = useMutationInitContract()
+
+  const onClickCreate = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
     setState({ key: 'hasTriedSubmitting', data: true })
 
     if (hasErrors) {
       return
     }
+
+    if (!isConnected) {
+      try {
+        await connectWallet()
+        await getAccounts()
+      } catch (error) {
+        console.log(error)
+        return
+      }
+    }
+
+    const initMsg = formatter({
+      name,
+      symbol,
+      decimals,
+      adminAddress,
+      enablePublicTokenSupply,
+      enableDeposit,
+      enableRedeem,
+      enableMint,
+      enableBurn,
+      initialBalances,
+    })
+
+    mutate({
+      codeId: CONTRACT_CODE_ID.SNIP20,
+      initMsg,
+      label: `${name} Snip20 ${cryptoRandomString({ length: 15 })}`,
+      maxGas: MAX_GAS.SNIP20_INIT_MSG,
+    })
   }
 
   return (
@@ -39,9 +103,12 @@ const CreatePage = () => {
           <Form>
             <InitialBalances />
             <Review />
-            <Button isStretched onClick={onClickCreate}>
-              Create
-            </Button>
+            <ButtonWithLoading
+              text="Create"
+              isStretched
+              onClick={onClickCreate}
+              loading={connecting || gettingAccounts || isLoading}
+            />
           </Form>
         </Content>
       </InnerContainer>
