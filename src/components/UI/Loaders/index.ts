ec9e4f672e62901f376832cbc25b695ac69c67f1
hieulai <>
2021-05-15 16:52:15 +1000

    💄 add styled dots

---
+++ src/components/UI/Loaders/index.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+import Dots from './Dots'
+
+const StyledDots = styled(Dots)`
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+`
+
+export { StyledDots }
