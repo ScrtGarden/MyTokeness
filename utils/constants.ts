3284485a30a99511d5fd52361b2b60e92c88fffd
hieulai <>
2021-05-17 10:18:34 +1000

    ✨ add sidebar tab

--- utils/constants.ts
+++ utils/constants.ts
@@ -10,4 +10,47 @@ const CONTRACT_CODE_ID = {
   // SNIP20: 0,
 }
 
-export { MAX_GAS, CONTRACT_CODE_ID }
+const SIDEBAR_TABS = {
+  '/': {
+    label: 'Home',
+    icon: 'home-duo',
+    route: '/',
+  },
+  create: {
+    label: 'Create',
+    icon: 'industry-duo',
+    route: '/create',
+  },
+  manage: {
+    label: 'Manage',
+    icon: 'tasks-alt-duo',
+    menu: [
+      {
+        label: 'Mint',
+        icon: 'hand-holding-usd',
+        route: '/manage/mint',
+        as: undefined,
+      },
+      {
+        label: 'Burn',
+        icon: 'fire-duo',
+        route: '/manage/burn',
+        as: undefined,
+      },
+      {
+        label: 'Minters',
+        icon: 'users-duo',
+        route: '/manage/minters',
+        as: undefined,
+      },
+      {
+        label: 'Admin',
+        icon: 'user-crown-duo',
+        route: '/manage/admin',
+        as: undefined,
+      },
+    ],
+  },
+}
+
+export { MAX_GAS, CONTRACT_CODE_ID, SIDEBAR_TABS }
