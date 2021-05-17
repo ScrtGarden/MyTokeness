c4f94a235856c83d9c8a89c31ae1d89443e6df5e
hieulai <>
2021-05-17 15:44:44 +1000

    ✨ add minters route

---
+++ pages/manage/minters.tsx
@@ -0,0 +1,8 @@
+import { getLayout } from '../../src/components/Layouts/SiteLayout'
+import Minters from '../../src/components/ManagePage/Minters'
+
+const Page = () => <Minters />
+
+Page.getLayout = getLayout
+
+export default Page
