f920a7e9d2ba846117ec6f599cf61e5e40684889
hieulai <>
2021-05-18 15:56:34 +1000

    🎨 update naming

--- utils/secretjs.ts
+++ utils/secretjs.ts
@@ -1,7 +1,7 @@
 import { CosmWasmClient } from 'secretjs'
 
-const queryClient = new CosmWasmClient(
+const queryChain = new CosmWasmClient(
   process.env.NEXT_PUBLIC_REST_URL as string
 )
 
-export { queryClient }
+export { queryChain }
