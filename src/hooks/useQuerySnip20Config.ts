9a1300ee45f60039646ab57a795d0da30008ece9
hieulai <>
2021-05-19 10:40:56 +1000

    ✨ add hook that get snip20 token config

---
+++ src/hooks/useQuerySnip20Config.ts
@@ -0,0 +1,21 @@
+import { UseQueryOptions, useQuery } from 'react-query'
+
+import { ResultTokenConfig } from '../../interface/snip20'
+import { queryChain } from '../../utils/secretjs'
+
+const useQuerySnip20Config = (
+  contractAddress: string,
+  options?: UseQueryOptions<ResultTokenConfig, Error>
+) =>
+  useQuery(
+    ['snip20', 'tokenConfig', contractAddress],
+    () => queryChain.queryContractSmart(contractAddress, { token_config: {} }),
+    {
+      enabled: !!contractAddress,
+      refetchOnWindowFocus: false,
+      retry: 1,
+      ...options,
+    }
+  )
+
+export default useQuerySnip20Config
