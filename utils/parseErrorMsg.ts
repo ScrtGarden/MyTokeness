c936e31241f068a876592976bacb26a6e5cf31e2
hieulai <>
2021-05-19 14:39:21 +1000

    ✨ better error msg parsing

--- utils/parseErrorMsg.ts
+++ utils/parseErrorMsg.ts
@@ -1,7 +1,13 @@
-type ErrorObj = { message?: string } | undefined
+import { getErrorMsgPattern } from './regexPatterns'
 
 const parseErrorMsg = (error: Error) => {
   const { message } = error
+
+  const result = message.match(getErrorMsgPattern)
+  if (result) {
+    const parsed = JSON.parse(result[0])
+    return parsed.generic_err.msg
+  }
 
   if (message.includes('out of gas')) {
     return 'You ran out of gas.'
