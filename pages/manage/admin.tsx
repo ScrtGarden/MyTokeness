e198cc725cb6d4e07deb39118ee5bea28e979c8e
hieulai <>
2021-05-19 10:39:35 +1000

    ✨ add admin route

---
+++ pages/manage/admin.tsx
@@ -0,0 +1,8 @@
+import { getLayout } from '../../src/components/Layouts/SiteLayout'
+import Admin from '../../src/components/ManagePage/Admin'
+
+const Page = () => <Admin />
+
+Page.getLayout = getLayout
+
+export default Page
