31e75538f47985d2317d9000dc21e41a9127f94c
hieulai <>
2021-05-19 14:50:56 +1000

    ✨ refetch wallet balance after contract execution

--- src/hooks/useMutationExeContract.ts
+++ src/hooks/useMutationExeContract.ts
@@ -1,14 +1,23 @@
-import { useMutation } from 'react-query'
+import { useMutation, useQueryClient } from 'react-query'
 import { ExecuteResult } from 'secretjs'
 
 import {
   ExecuteContractParams,
   executeContract,
 } from '../../utils/contractInteractions'
+import { useStoreState } from './storeHooks'
 
 const useMutationExeContract = <T extends object>() => {
+  const queryClient = useQueryClient()
+  const walletAddress = useStoreState((state) => state.auth.connectedAddress)
+
   return useMutation<ExecuteResult, Error, ExecuteContractParams<T>, unknown>(
-    (params: ExecuteContractParams<T>) => executeContract(params)
+    (params: ExecuteContractParams<T>) => executeContract(params),
+    {
+      onSettled: () => {
+        queryClient.invalidateQueries(['nativeBalance', walletAddress])
+      },
+    }
   )
 }
 
