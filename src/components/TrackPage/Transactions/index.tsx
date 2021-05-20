ff206102e576e70174f2ea7b5eb8359f3814ea78
hieulai <>
2021-05-20 17:01:14 +1000

    ✨ get transaction history data

---
+++ src/components/TrackPage/Transactions/index.tsx
@@ -0,0 +1,147 @@
+import { useEffect, useMemo, useState } from 'react'
+import { useQueryClient } from 'react-query'
+
+import {
+  QueryTransactionHistory,
+  ResultTransactionHistory,
+  Tx,
+} from '../../../../interface/snip20'
+import { useStoreState } from '../../../hooks/storeHooks'
+import useDebounce from '../../../hooks/useDebounce'
+import useQueryContract from '../../../hooks/useQueryContract'
+import useQuerySnip20Config from '../../../hooks/useQuerySnip20Config'
+import useQuerySnip20ViewingKey from '../../../hooks/useQuerySnip20ViewingKey'
+import Snip20Selector from '../../Cards/Snip20Selector'
+import { Container, InnerContainer } from '../../UI/Containers'
+import { PageTitle } from '../../UI/Typography'
+import Table from './Table'
+
+const Transactions = () => {
+  const queryClient = useQueryClient()
+
+  // store state
+  const walletAddress = useStoreState((state) => state.auth.connectedAddress)
+
+  // component state
+  const [contractAddress, setContractAddress] = useState(
+    queryClient.getQueryData('selectedContractAddress') || ''
+  )
+  const debouncedAddy = useDebounce(contractAddress, 300)
+  const [error, setError] = useState('')
+  const [page, setPage] = useState(1)
+
+  // custom hook - query snip20 config's
+  const { isLoading, isSuccess } = useQuerySnip20Config(debouncedAddy, {
+    onSuccess: () => {
+      queryClient.setQueryData('selectedContractAddress', debouncedAddy)
+    },
+    onError: () => {
+      setError('Unable to fetch token information.')
+    },
+  })
+
+  // custom hook - only query viewing key if connected to keplr and snip20
+  // config is valid
+  const enabled = useMemo(
+    () => !!(walletAddress && contractAddress && isSuccess),
+    [walletAddress, contractAddress, isSuccess]
+  )
+  const { data: viewingKey, isLoading: gettingKey } = useQuerySnip20ViewingKey(
+    { walletAddress, contractAddress: debouncedAddy },
+    {
+      enabled,
+      onError: () => {
+        setError("Can't find viewing key from Keplr Wallet.")
+      },
+    }
+  )
+
+  // custom hook - only query transaction history if viewing key is valid
+  const { data } = useQueryContract<
+    QueryTransactionHistory,
+    ResultTransactionHistory,
+    Tx[]
+  >(
+    ['transactionHistory', walletAddress, contractAddress, page],
+    contractAddress,
+    {
+      transaction_history: {
+        address: walletAddress,
+        key: viewingKey as string,
+        page: page,
+        page_size: 10,
+      },
+    },
+    {
+      enabled: !!viewingKey,
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
+      select: (data) => data.transaction_history.txs,
+    }
+  )
+
+  // lifecycle
+  useEffect(() => {
+    if (error) {
+      setError('')
+    }
+  }, [debouncedAddy, viewingKey])
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  return (
+    <Container>
+      <InnerContainer>
+        <PageTitle>Transfers</PageTitle>
+        <Snip20Selector
+          value={contractAddress}
+          debouncedValue={debouncedAddy}
+          onChange={(e) => setContractAddress(e.currentTarget.value)}
+          loading={isLoading || gettingKey}
+          error={error}
+        />
+        <Table columns={columns} data={data} />
+      </InnerContainer>
+    </Container>
+  )
+}
+
+export default Transactions
