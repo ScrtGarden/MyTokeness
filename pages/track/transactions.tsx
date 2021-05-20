e68cee1230573188d30b968af7796fe1140f521a
hieulai <>
2021-05-20 16:56:34 +1000

    ✨ add transactions route

---
+++ pages/track/transactions.tsx
@@ -0,0 +1,8 @@
+import { getLayout } from '../../src/components/Layouts/SiteLayout'
+import Transactions from '../../src/components/TrackPage/Transactions'
+
+const Page = () => <Transactions />
+
+Page.getLayout = getLayout
+
+export default Page
