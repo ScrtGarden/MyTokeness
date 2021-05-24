bd5b49857f042acc62023740d78175251e794ff2
hieulai <>
2021-05-24 14:01:34 +1000

    ✨ add sections

--- src/components/Layouts/SiteLayout/Sidebar/index.tsx
+++ src/components/Layouts/SiteLayout/Sidebar/index.tsx
@@ -1,11 +1,17 @@
 import { SIDEBAR_TABS } from '../../../../../utils/constants'
-import { Container } from './styles'
+import { Container, SectionHeader } from './styles'
 import Tab from './Tab'
 
 const Sidebar = () => (
   <Container>
-    {Object.entries(SIDEBAR_TABS).map(([key, value]) => (
-      <Tab key={key} item={value} id={key} />
+    <Tab item={SIDEBAR_TABS.HOME} />
+    <SectionHeader>TOKEN</SectionHeader>
+    {Object.entries(SIDEBAR_TABS.TOKEN).map(([key, value]) => (
+      <Tab key={key} item={value} />
+    ))}
+    <SectionHeader>NFT</SectionHeader>
+    {Object.entries(SIDEBAR_TABS.NFT).map(([key, value]) => (
+      <Tab key={key} item={value} />
     ))}
   </Container>
 )
