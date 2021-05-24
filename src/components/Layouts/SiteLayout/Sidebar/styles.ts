5061c85889c6be9f841e4e5800086c4ec9c171df
hieulai <>
2021-05-24 14:02:01 +1000

    💄 add section header / add scroll overflow

--- src/components/Layouts/SiteLayout/Sidebar/styles.ts
+++ src/components/Layouts/SiteLayout/Sidebar/styles.ts
@@ -4,10 +4,19 @@ const Container = styled.div`
   align-self: flex-start;
   background-color: ${(props) => props.theme.fg};
   border-right: 2px solid ${(props) => props.theme.border.color};
+  height: calc(100vh - 62px);
+  overflow: scroll;
+  padding: ${(props) => props.theme.space.sm} 0;
   position: sticky;
   top: 62px;
-  height: calc(100vh - 62px);
-  padding: ${(props) => props.theme.space.sm} 0;
 `
 
-export { Container }
+const SectionHeader = styled.div`
+  background: ${(props) => props.theme.bg};
+  color: ${(props) => props.theme.font.colors.secondary};
+  font-size: ${(props) => props.theme.font.sizes.sm};
+  font-weight: ${(props) => props.theme.font.weights.bold};
+  padding: ${(props) => `${props.theme.space.xs} ${props.theme.space.md}`};
+`
+
+export { Container, SectionHeader }
