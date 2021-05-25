52a573d7e227efa51bde6e9e59c1e9969cb144d4
hieulai <>
2021-05-25 13:44:11 +1000

    ✨ invalidate balance on settled

--- src/hooks/useMutationInitContract.ts
+++ src/hooks/useMutationInitContract.ts
@@ -1,12 +1,21 @@
-import { UseMutationResult, useMutation } from 'react-query'
+import { UseMutationResult, useMutation, useQueryClient } from 'react-query'
 import { InstantiateResult } from 'secretjs'
 
 import { Params, instantiateContract } from '../../utils/contractInteractions'
+import { useStoreState } from './storeHooks'
 
-const useMutationInitContract = (): UseMutationResult<
+const useMutationInitContract = <T extends object>(): UseMutationResult<
   InstantiateResult,
   Error,
-  Params
-> => useMutation(instantiateContract)
+  Params<T>
+> => {
+  const queryClient = useQueryClient()
+  const walletAddress = useStoreState((state) => state.auth.connectedAddress)
+
+  return useMutation((data) => instantiateContract(data), {
+    onSettled: () =>
+      queryClient.invalidateQueries(['nativeBalance', walletAddress]),
+  })
+}
 
 export default useMutationInitContract
