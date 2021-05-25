ee59c0cb8c2ecfaed09a579d00eb2d3142353567
hieulai <>
2021-05-25 17:12:17 +1000

    🔥 clean up unused component

--- src/components/Modals/ConfigureCollection/Form/styles.ts
+++ src/components/Modals/ConfigureCollection/Form/styles.ts
@@ -2,14 +2,8 @@ import styled from 'styled-components'
 
 import { Button } from '../../../UI/Buttons'
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: ${(props) => props.theme.space.md};
-`
-
 const StyledButton = styled(Button)`
   margin-top: ${(props) => props.theme.space.lg};
 `
 
-export { Container, StyledButton }
+export { StyledButton }
