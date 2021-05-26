d0a92ed65a69211333a9bad3ed6abe93dd43ac5f
hieulai <>
2021-05-26 20:46:22 +1000

    🚚 reorganise routes

--- pages/nft/collections.tsx
+++
@@ -1,8 +0,0 @@
-import Collections from '../../src/components/CollectionsPage'
-import { getLayout } from '../../src/components/Layouts/SiteLayout'
-
-const Page = () => <Collections />
-
-Page.getLayout = getLayout
-
-export default Page
---
+++ pages/nft/collections/[contractAddress].tsx
@@ -0,0 +1,8 @@
+import CollectionPage from '../../../src/components/CollectionPage'
+import { getLayout } from '../../../src/components/Layouts/SiteLayout'
+
+const Page = () => <CollectionPage />
+
+Page.getLayout = getLayout
+
+export default Page
