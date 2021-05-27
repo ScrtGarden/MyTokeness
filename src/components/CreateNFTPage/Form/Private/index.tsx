371476da16ff60f92a62b96951752259cd38666a
hieulai <>
2021-05-27 16:37:22 +1000

    ✨ add private form

--- src/components/CreateNFTPage/Form/Private/index.tsx
+++ src/components/CreateNFTPage/Form/Private/index.tsx
@@ -1,13 +1,46 @@
 import { memo } from 'react'
 
-import { Card, Header } from '../../../UI/Card'
+import ButtonWithLoading from '../../../Common/ButtonWithLoading'
+import { Card, Header, Wrapper } from '../../../UI/Card'
+import { Field, Label, Textarea } from '../../../UI/Forms'
+import ContextStore from '../../Store'
+import FileUploader from '../FileUploader'
 
-const Public = () => {
+const Private = () => {
+  // context store state
+  const content = ContextStore.useStoreState(
+    (state) => state.privateMetadata.content
+  )
+
+  // context store actions
+  const setFile = ContextStore.useStoreActions(
+    (actions) => actions.setPrivateFile
+  )
+  const setContent = ContextStore.useStoreActions(
+    (actions) => actions.setPrivateMetadata
+  )
+
   return (
     <Card>
-      <Header>Private Data</Header>
+      <Header>Owner's Eyes Only</Header>
+      <Wrapper>
+        <FileUploader
+          label="Image, Video or Audio (optional)"
+          setFile={setFile}
+        />
+        <Field>
+          <Label>Content (optional)</Label>
+          <Textarea
+            rows={5}
+            placeholder="Access key, code to nuclear warheads, link to file, etc...Charmander can be found outside of rock tunnel in fire red."
+            value={content}
+            onChange={(e) => setContent(e.currentTarget.value)}
+          />
+        </Field>
+        <ButtonWithLoading text="Create" isPrimary />
+      </Wrapper>
     </Card>
   )
 }
 
-export default memo(Public)
+export default memo(Private)
