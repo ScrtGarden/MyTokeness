ecd831f857fb5ddf218a219350d3c040836b1a06
hieulai <>
2021-05-27 10:08:15 +1000

    ✨ add create route

---
+++ pages/nft/collections/[contractAddress]/create.tsx
@@ -0,0 +1,8 @@
+import CreateNFTPage from '../../../../src/components/CreateNFTPage'
+import { getLayout } from '../../../../src/components/Layouts/SiteLayout'
+
+const Page = () => <CreateNFTPage />
+
+Page.getLayout = getLayout
+
+export default Page
