4fba177923b5adf44c0bcee3bdaa7cf8c19e90d9
hieulai <>
2021-05-28 14:42:21 +1000

    🎨 clean up code

--- src/components/CreateNFTPage/Form/FileUploader/FileUploadContent/Preview/index.tsx
+++ src/components/CreateNFTPage/Form/FileUploader/FileUploadContent/Preview/index.tsx
@@ -8,27 +8,22 @@ type Props = {
   type: string
 }
 
-const Preview: FC<Props> = (props) => {
-  const { src, type } = props
-  console.log({ src, type })
-
-  return (
-    <Container>
-      {type.includes('image/') ? (
-        <Image src={src} />
-      ) : (
-        <StyledPlayer>
-          <ReactPlayer
-            className="react-player"
-            url={src}
-            width="100%"
-            height="100%"
-            controls
-          />
-        </StyledPlayer>
-      )}
-    </Container>
-  )
-}
+const Preview: FC<Props> = ({ src, type }) => (
+  <Container>
+    {type.includes('image/') ? (
+      <Image src={src} />
+    ) : (
+      <StyledPlayer>
+        <ReactPlayer
+          className="react-player"
+          url={src}
+          width="100%"
+          height="100%"
+          controls
+        />
+      </StyledPlayer>
+    )}
+  </Container>
+)
 
 export default memo(Preview)
