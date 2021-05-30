49d4c6ac83b938e141857e1c860eeb5b88fbdeaa
hieulai <>
2021-05-30 14:55:28 +1000

    ✨ format data for handle msg

--- src/components/Modals/NFTMintingSteps/lib.ts
+++ src/components/Modals/NFTMintingSteps/lib.ts
@@ -1,34 +1,22 @@
 import cryptoRandomString from 'crypto-random-string'
 
-import { InitMsg } from '../../../../interface/nft'
+import {
+  HandleBatchMintNFT,
+  HandleMintNFT,
+  InitMsg,
+} from '../../../../interface/nft'
 import { DraftCollectionConfig } from '../../../../store/collections/collections.model'
-import { Attribute, MetadataUI, PrivateMetadataUI } from '../Store/model'
+import {
+  Attribute,
+  PrivateMetadata,
+  PublicMetadata,
+} from '../../CreateNFTPage/Store/model'
 
-const formatAttrs = (attrs: Attribute[]) =>
-  attrs.filter(({ key, value }) => key && value)
-
-type Params = {
-  publicMetadata: MetadataUI
-  privateMetadata: PrivateMetadataUI
-  file: File
-  ipfsHash: string
-}
-
-const formatter = (params: Params) => {
-  const { privateMetadata, publicMetadata, file, ipfsHash } = params
-  const { name, description = '', attributes } = publicMetadata
-
-  const formattedPublic = {
-    name: name.trim(),
-    description: description.trim(),
-    attributes: JSON.stringify(formatAttrs(attributes)),
-    image: `ipfs://${ipfsHash}/${file.name}`,
-  }
-
-  return {
-    formattedPublic,
-    formattedPrivate: privateMetadata,
-  }
+interface FormatForHandleMsgParams {
+  publicMetadata: PublicMetadata
+  publicFileLink: string
+  privateMetadata: PrivateMetadata
+  privateFileLink: string
 }
 
 const formatForInstantiateMsg = (data: DraftCollectionConfig): InitMsg => {
@@ -60,4 +48,95 @@ const formatForInstantiateMsg = (data: DraftCollectionConfig): InitMsg => {
   }
 }
 
-export { formatter, formatAttrs, formatForInstantiateMsg }
+const formatAttrs = (attrs: Attribute[]) =>
+  attrs.filter(({ key, value }) => key && value)
+
+const formatSingleMint = ({
+  publicMetadata,
+  publicFileLink,
+  privateMetadata,
+  privateFileLink,
+}: FormatForHandleMsgParams): HandleMintNFT => {
+  const publicProperties = {
+    rarity: {
+      number: 1,
+      total: 1,
+    },
+  }
+  const privateProperties = {
+    content: privateMetadata.content,
+  }
+  const attributes = formatAttrs(publicMetadata.attributes)
+
+  return {
+    mint_nft: {
+      token_id: cryptoRandomString({ length: 21 }),
+      public_metadata: {
+        name: publicMetadata.name,
+        description: publicMetadata.description,
+        image: publicFileLink,
+        properties: JSON.stringify(publicProperties),
+        attributes: JSON.stringify(attributes),
+      },
+      private_metadata: {
+        ...(privateFileLink ? { image: privateFileLink } : {}),
+        properties: JSON.stringify(privateProperties),
+      },
+    },
+  }
+}
+
+const formatBatchMint = ({
+  publicMetadata,
+  publicFileLink,
+  privateMetadata,
+  privateFileLink,
+}: FormatForHandleMsgParams): HandleBatchMintNFT => {
+  const supply = parseInt(publicMetadata.supply, 10)
+  const rarityId = cryptoRandomString({ length: 21 })
+  const privateProperties = {
+    content: privateMetadata.content,
+  }
+  const attributes = formatAttrs(publicMetadata.attributes)
+  const mints = []
+
+  for (let i = 0; i < supply; i++) {
+    const publicProperties = {
+      rarity: {
+        id: rarityId,
+        number: i + 1,
+        total: supply,
+      },
+    }
+
+    mints.push({
+      token_id: cryptoRandomString({ length: 21 }),
+      public_metadata: {
+        name: publicMetadata.name,
+        description: publicMetadata.description,
+        image: publicFileLink,
+        properties: JSON.stringify(publicProperties),
+        attributes: JSON.stringify(attributes),
+      },
+      private_metadata: {
+        ...(privateFileLink ? { image: privateFileLink } : {}),
+        properties: JSON.stringify(privateProperties),
+      },
+    })
+  }
+
+  return {
+    batch_mint_nft: {
+      mints,
+    },
+  }
+}
+
+const formatForHandleMsg = (
+  data: FormatForHandleMsgParams
+): HandleMintNFT | HandleBatchMintNFT =>
+  data.publicMetadata.supply === '1'
+    ? formatSingleMint(data)
+    : formatBatchMint(data)
+
+export { formatAttrs, formatForInstantiateMsg, formatForHandleMsg }
