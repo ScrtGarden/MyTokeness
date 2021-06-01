bf8dcd3825bb8b60d762cfbd006dda6240a69a82
hieulai <>
2021-06-01 21:13:25 +1000

    ✨ add infinite query nft

---
+++ src/hooks/useInfiniteQueryTokens.ts
@@ -0,0 +1,32 @@
+import { useInfiniteQuery } from 'react-query'
+
+import { QueryTokens, ResultTokens } from '../../interface/nft'
+import { queryChain } from '../../utils/secretjs'
+
+const useInfiniteQueryTokens = (
+  walletAddress: string,
+  contractAddress: string,
+  queryMsg: QueryTokens['tokens']
+) => {
+  return useInfiniteQuery<ResultTokens, Error>(
+    ['tokens', walletAddress, contractAddress],
+    ({ pageParam = '' }) =>
+      queryChain.queryContractSmart(contractAddress, {
+        tokens: { ...queryMsg, start_after: pageParam },
+      }),
+    {
+      refetchOnWindowFocus: false,
+      getNextPageParam: (lastPage) => {
+        const length = lastPage.token_list.tokens.length
+        const limit = queryMsg.limit || 10
+        return length < limit
+          ? undefined
+          : lastPage.token_list.tokens[length - 1]
+      },
+
+      enabled: !!queryMsg.viewing_key,
+    }
+  )
+}
+
+export default useInfiniteQueryTokens
