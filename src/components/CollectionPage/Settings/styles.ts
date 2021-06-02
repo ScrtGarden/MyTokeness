2457871adee42ef9fd66f98f44ae518d11009025
hieulai <>
2021-06-02 11:45:52 +1000

    💄 styles for settings page

--- src/components/CollectionPage/Settings/styles.ts
+++ src/components/CollectionPage/Settings/styles.ts
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-  border: 1px solid red;
+  display: flex;
+  flex-direction: row;
   color: #fff;
+  padding-top: ${(props) => props.theme.space.sm};
+  width: 100%;
 `
 
 export { Container }
