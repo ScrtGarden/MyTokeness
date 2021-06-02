1307ef956ceca4c864318bd43875eb51ef84d345
hieulai <>
2021-06-02 15:30:40 +1000

    ✨ add audio/video player

--- src/components/Cards/NFT/Visual/index.tsx
+++ src/components/Cards/NFT/Visual/index.tsx
@@ -1,6 +1,7 @@
 import mime from 'mime-types'
 import Image from 'next/image'
-import { FC, memo, useMemo } from 'react'
+import { FC, FormEvent, memo, useMemo } from 'react'
+import ReactPlayer from 'react-player'
 
 import { hashLinkPattern } from '../../../../../utils/regexPatterns'
 import { ImageWrapper } from './styles'
@@ -10,22 +11,49 @@ type Props = {
 }
 
 const Visual: FC<Props> = ({ image }) => {
-  const file = useMemo(() => {
+  const { type, hashLink } = useMemo(() => {
     const hashLink = image.match(hashLinkPattern)
+    const mimeType = mime.lookup(image)
     return {
-      type: mime.lookup(image),
+      type: mimeType ? mimeType.split('/')[0] : undefined,
       hashLink: hashLink ? hashLink[0] : '',
     }
   }, [image])
 
   return (
-    <ImageWrapper>
-      <Image
-        className="next-image"
-        src={`https://ipfs.io/ipfs/${file.hashLink}`}
-        layout="fill"
-        objectFit="cover"
-      />
+    <ImageWrapper type={type}>
+      {type &&
+        (type === 'image' ? (
+          <Image
+            className="next-image"
+            src={`https://ipfs.io/ipfs/${hashLink}`}
+            layout="fill"
+            objectFit="cover"
+          />
+        ) : (
+          <ReactPlayer
+            className="react-player"
+            url={`https://ipfs.io/ipfs/${hashLink}`}
+            width="100%"
+            height="100%"
+            controls
+            light={type === 'audio' ? '/images/audio-thumbnail.png' : undefined}
+            config={{
+              file: {
+                attributes: {
+                  controlsList: 'nodownload',
+                  disablePictureInPicture: true,
+                  onContextMenu: (e: FormEvent) => e.preventDefault(),
+                  ...(type === 'audio'
+                    ? {
+                        poster: '/images/audio-thumbnail.png',
+                      }
+                    : {}),
+                },
+              },
+            }}
+          />
+        ))}
     </ImageWrapper>
   )
 }
--- src/components/Cards/NFT/Visual/styles.ts
+++ src/components/Cards/NFT/Visual/styles.ts
@@ -1,11 +1,23 @@
 import styled from 'styled-components'
 
-const ImageWrapper = styled.div`
+interface ContainerProps {
+  readonly type?: string
+}
+
+const ImageWrapper = styled.div.attrs<ContainerProps>(({ type }) => ({
+  style: {
+    backgroundColor: type === 'video' ? '#000' : 'transparent',
+  },
+}))<ContainerProps>`
+  background-color: #000;
   aspect-ratio: 1.3;
   position: relative;
   width: 100%;
 
   .next-image {
+  }
+
+  .react-player {
   }
 `
 
