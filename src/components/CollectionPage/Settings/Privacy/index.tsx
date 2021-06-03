6551208eca7a99135d5d38c79654f9daa2ab1c01
hieulai <>
2021-06-03 15:41:25 +1000

    ✨ fetch inventory approval data

--- src/components/CollectionPage/Settings/Privacy/index.tsx
+++ src/components/CollectionPage/Settings/Privacy/index.tsx
@@ -1,7 +1,69 @@
+import { useRouter } from 'next/router'
+
+import {
+  QueryInventoryApprovals,
+  ResultInventoryApprovals,
+} from '../../../../../interface/nft'
+import { useStoreState } from '../../../../hooks/storeHooks'
+import useQueryContract from '../../../../hooks/useQueryContract'
+import ApprovalSetting from '../../../Cards/ApprovalSetting'
+import { CollectionRouterQuery } from '../../../Layouts/CollectionLayout'
 import { Container } from '../styles'
 
 const Privacy = () => {
-  return <Container>Privacy</Container>
+  const router = useRouter()
+  const { contractAddress } = router.query as CollectionRouterQuery
+
+  const walletAddress = useStoreState((state) => state.auth.connectedAddress)
+  const viewingKey = useStoreState((state) =>
+    state.auth.keyByContractAddress(contractAddress)
+  )
+
+  // custom hooks
+  const { data, isLoading } = useQueryContract<
+    QueryInventoryApprovals,
+    ResultInventoryApprovals
+  >(
+    ['inventoryApprovals', walletAddress, contractAddress],
+    contractAddress,
+    {
+      inventory_approvals: { address: walletAddress, viewing_key: viewingKey },
+    },
+    { enabled: !!viewingKey }
+  )
+
+  console.log({ data })
+
+  if (isLoading) {
+    return <Container>Loading...</Container>
+  }
+
+  if (!data) {
+    return null
+  }
+
+  return (
+    <Container>
+      <ApprovalSetting
+        title="Ownership Privacy Setting"
+        description="Turning this off will allow anyone see which assets you own."
+        isPrivate={!data.inventory_approvals.owner_is_public}
+        expiration={data.inventory_approvals.public_ownership_expiration}
+        toggleId="ownership"
+        toggleLabel="Hide ownership"
+      />
+      <ApprovalSetting
+        title="Private Metadata Privacy Setting"
+        description="Turning this off will allow anyone see the private metadata of all your owned assets."
+        isPrivate={!data.inventory_approvals.private_metadata_is_public}
+        expiration={
+          data.inventory_approvals.private_metadata_is_public_expiration
+        }
+        toggleId="privateMetadata"
+        toggleLabel="Hide private metadata"
+      />
+    </Container>
+  )
 }
 
 export default Privacy
--- src/components/CollectionPage/Settings/styles.ts
+++ src/components/CollectionPage/Settings/styles.ts
@@ -2,9 +2,10 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   color: #fff;
   padding-top: ${(props) => props.theme.space.sm};
+  row-gap: ${(props) => props.theme.space.lg};
   width: 100%;
 `
 
