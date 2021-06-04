b5a1e6470447e4e4194168ca99b2f1f575d3cb45
hieulai <>
2021-06-04 10:15:49 +1000

    ♻️ use expiration form

--- src/components/Cards/ApprovalSetting/index.tsx
+++ src/components/Cards/ApprovalSetting/index.tsx
@@ -1,35 +1,15 @@
-import {
-  FC,
-  FormEvent,
-  memo,
-  useEffect,
-  useMemo,
-  useReducer,
-  useState,
-} from 'react'
-import DatePicker from 'react-datepicker'
+import { FC, memo, useEffect, useMemo, useReducer, useState } from 'react'
 
 import { Expiration } from '../../../../interface/nft'
 import reducer from '../../../../utils/reducer'
-import { blockheightPattern } from '../../../../utils/regexPatterns'
 import ButtonWithLoading from '../../Common/ButtonWithLoading'
-import MessageWithIcon from '../../Common/MessageWithIcon'
+import ExpirationForm from '../../Common/ExpirationForm'
 import { Header, SettingsCard } from '../../UI/Card'
-import { DatePickerWrapper } from '../../UI/DatePicker'
-import { Label } from '../../UI/Forms'
-import { Radio } from '../../UI/Forms/Radio'
+import { Label, ToggleWrapper } from '../../UI/Forms'
 import Toggle from '../../UI/Forms/Toggle'
 import { Text } from '../../UI/Typography'
 import { FormatExpiration, formatExpiration } from './lib'
-import {
-  ButtonWrapper,
-  Content,
-  Field,
-  StyledInput,
-  StyledRadioGroup,
-  ToggleWrapper,
-  Wrapper,
-} from './styles'
+import { ButtonWrapper, Content } from './styles'
 
 type Reducer = (p: FormatExpiration, u: FormatExpiration) => FormatExpiration
 
@@ -81,13 +61,6 @@ const ApprovalSetting: FC<Props> = ({
     setErrorState(error)
   }, [error])
 
-  const onChangeBlockheight = (e: FormEvent<HTMLInputElement>) => {
-    const blockheight = e.currentTarget.value
-    if (!blockheight || blockheight.match(blockheightPattern)) {
-      setExpSettings({ blockheight })
-    }
-  }
-
   return (
     <SettingsCard>
       <Header>{title}</Header>
@@ -102,46 +75,11 @@ const ApprovalSetting: FC<Props> = ({
           <Label>{toggleLabel}</Label>
         </ToggleWrapper>
         {!isPrivateState && (
-          <Field>
-            <Label>Expiration</Label>
-            <StyledRadioGroup
-              selectedValue={expSettings.type}
-              onClick={(value) =>
-                setExpSettings({ type: value as FormatExpiration['type'] })
-              }
-            >
-              <Radio value="never" labelText="Never" isRegular />
-              <Radio value="date" labelText="Date" isRegular />
-              <Radio value="blockheight" labelText="Blockheight" isRegular />
-            </StyledRadioGroup>
-            {expSettings.type === 'blockheight' && (
-              <Wrapper>
-                <StyledInput
-                  placeholder="3000000"
-                  value={expSettings.blockheight}
-                  onChange={onChangeBlockheight}
-                />
-                {errorState && (
-                  <MessageWithIcon validation="error" message={errorState} />
-                )}
-              </Wrapper>
-            )}
-            {expSettings.type === 'date' && (
-              <DatePickerWrapper>
-                <DatePicker
-                  selected={expSettings.date}
-                  onChange={(date: Date) => setExpSettings({ date })}
-                  customInput={<StyledInput />}
-                  dateFormat="MMMM d, yyyy h:mm aa"
-                  showTimeInput
-                  minDate={new Date()}
-                />
-                {errorState && (
-                  <MessageWithIcon validation="error" message={errorState} />
-                )}
-              </DatePickerWrapper>
-            )}
-          </Field>
+          <ExpirationForm
+            settings={expSettings}
+            onChange={setExpSettings}
+            error={errorState}
+          />
         )}
       </Content>
       <ButtonWrapper>
--- src/components/Cards/ApprovalSetting/styles.ts
+++ src/components/Cards/ApprovalSetting/styles.ts
@@ -1,9 +1,5 @@
 import styled from 'styled-components'
 
-import ButtonWithLoading from '../../Common/ButtonWithLoading'
-import { Input } from '../../UI/Forms'
-import { RadioGroup } from '../../UI/Forms/Radio'
-
 const Content = styled.div`
   display: flex;
   flex-direction: column;
@@ -11,41 +7,9 @@ const Content = styled.div`
   row-gap: ${(props) => props.theme.space.md};
 `
 
-const ToggleWrapper = styled.div`
-  align-items: center;
-  column-gap: ${(props) => props.theme.space.md};
-  display: flex;
-  flex-direction: row;
-`
-
 const ButtonWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
 `
 
-const Field = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: ${(props) => props.theme.space.md};
-`
-
-const StyledRadioGroup = styled(RadioGroup)`
-  column-gap: ${(props) => props.theme.space.lg};
-  flex-direction: row;
-`
-
-const StyledInput = styled(Input)`
-  max-width: 350px;
-`
-
-const Wrapper = styled.div``
-
-export {
-  Content,
-  ToggleWrapper,
-  ButtonWrapper,
-  StyledRadioGroup,
-  StyledInput,
-  Field,
-  Wrapper,
-}
+export { Content, ButtonWrapper }
