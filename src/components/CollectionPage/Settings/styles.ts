0b7fbcbadbe8b32e2e0b3c29fc3784f7f2a87ef7
hieulai <>
2021-06-05 15:09:00 +1000

    💄 remove padding top

--- src/components/CollectionPage/Settings/styles.ts
+++ src/components/CollectionPage/Settings/styles.ts
@@ -4,7 +4,6 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   color: #fff;
-  padding-top: ${(props) => props.theme.space.sm};
   row-gap: ${(props) => props.theme.space.lg};
   width: 100%;
 `
