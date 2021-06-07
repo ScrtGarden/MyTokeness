d3be562756686c25a7df85b36eed780450a188f1
hieulai <>
2021-06-07 13:09:05 +1000

    ✨ can edit/ remove whitelisted address

--- src/components/Cards/WhitelistSetting/ApprovalList/Item/Editor/index.tsx
+++ src/components/Cards/WhitelistSetting/ApprovalList/Item/Editor/index.tsx
@@ -1,25 +1,49 @@
-import { FC, memo, useEffect, useReducer } from 'react'
+import { useRouter } from 'next/router'
+import { FC, memo, useEffect, useReducer, useState } from 'react'
+import { useQueryClient } from 'react-query'
+import { toast } from 'react-toastify'
 
+import {
+  HandleSetWhitelistedApproval,
+  ResultInventoryApprovals,
+} from '../../../../../../../interface/nft'
 import {
   ApprovalOptions,
   ApprovalOptionsReducer,
   ExpirationReducer,
   UIExpiration,
 } from '../../../../../../../interface/nft-ui'
+import { MAX_GAS } from '../../../../../../../utils/constants'
+import parseErrorMsg from '../../../../../../../utils/parseErrorMsg'
 import reducer from '../../../../../../../utils/reducer'
+import { useStoreState } from '../../../../../../hooks/storeHooks'
+import useMutationExeContract from '../../../../../../hooks/useMutationExeContract'
+import {
+  formatAdd,
+  updateTokenApprovals,
+} from '../../../../../CollectionPage/Settings/Privacy/lib'
 import ButtonWithLoading from '../../../../../Common/ButtonWithLoading'
+import { CollectionRouterQuery } from '../../../../../Layouts/CollectionLayout'
 import { Button } from '../../../../../UI/Buttons'
 import Permissions from '../../../Permissions'
+import { validate } from './lib'
 import { Buttons, Cell, Content, Wrapper } from './styles'
 
 type Props = {
+  address: string
   options: ApprovalOptions
   expiration: UIExpiration
   toggle: () => void
 }
 
 const Editor: FC<Props> = (props) => {
-  const { expiration, options, toggle } = props
+  const { expiration, options, toggle, address } = props
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  const { contractAddress } = router.query as CollectionRouterQuery
+
+  // store state
+  const walletAddress = useStoreState((state) => state.auth.connectedAddress)
 
   // component state
   const [localOptions, setLocalOptions] = useReducer<ApprovalOptionsReducer>(
@@ -30,6 +54,12 @@ const Editor: FC<Props> = (props) => {
     reducer,
     expiration
   )
+  const [isUpdate, setIsUpdate] = useState<boolean | undefined>(false)
+  const [error, setError] = useState('')
+
+  // custom hooks
+  const { mutate, isLoading } =
+    useMutationExeContract<HandleSetWhitelistedApproval>()
 
   // lifecycle
   useEffect(() => {
@@ -40,6 +70,67 @@ const Editor: FC<Props> = (props) => {
     setLocalExpiration(expiration)
   }, [expiration])
 
+  const onClick = (update?: boolean) => {
+    setIsUpdate(update)
+
+    const { errors, hasError } = validate(localOptions, localExpiration)
+
+    setError(errors.expiration)
+
+    if (hasError) {
+      return
+    }
+
+    const handleMsg: HandleSetWhitelistedApproval = update
+      ? formatAdd(address, localOptions, localExpiration)
+      : {
+          set_whitelisted_approval: {
+            address,
+            view_owner: 'none',
+            view_private_metadata: 'none',
+            transfer: 'none',
+          },
+        }
+
+    mutate(
+      {
+        contractAddress,
+        maxGas: MAX_GAS.NFT.SET_WHITELIST_APPROVAL,
+        handleMsg,
+      },
+      {
+        onSuccess: (_, { handleMsg: { set_whitelisted_approval } }) => {
+          const key = ['inventoryApprovals', walletAddress, contractAddress]
+          const original =
+            queryClient.getQueryData<ResultInventoryApprovals>(key)
+
+          if (original) {
+            const { inventory_approvals } = original.inventory_approvals
+            const updatedApprovals = updateTokenApprovals(
+              inventory_approvals,
+              set_whitelisted_approval
+            )
+
+            queryClient.setQueryData<ResultInventoryApprovals>(key, {
+              inventory_approvals: {
+                ...original.inventory_approvals,
+                inventory_approvals: updatedApprovals,
+              },
+            })
+          }
+
+          toggle()
+          toast.success(
+            update ? 'Updated whitelist.' : 'Removed address from list.'
+          )
+        },
+        onError: (error) => {
+          toast.error(parseErrorMsg(error))
+        },
+      }
+    )
+  }
+
   return (
     <Cell colSpan={4}>
       <Content>
@@ -49,12 +140,25 @@ const Editor: FC<Props> = (props) => {
           expiration={localExpiration}
           setOptions={setLocalOptions}
           setExpiration={setLocalExpiration}
+          error={error}
         />
         <Buttons>
-          <ButtonWithLoading text="Remove" isDanger />
+          <ButtonWithLoading
+            width={79}
+            text="Remove"
+            isDanger
+            loading={isLoading && !isUpdate}
+            onClick={() => onClick()}
+          />
           <Wrapper>
             <Button onClick={toggle}>Cancel</Button>
-            <ButtonWithLoading text="Save" isPrimary />
+            <ButtonWithLoading
+              text="Save"
+              isPrimary
+              width={56}
+              loading={isLoading && isUpdate}
+              onClick={() => onClick(true)}
+            />
           </Wrapper>
         </Buttons>
       </Content>
