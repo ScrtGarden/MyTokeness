c85db7c0ff37d3f80dde95d0249db26ce1943427
hieulai <>
2021-06-08 15:06:03 +1000

    💄 add pagination skeleton

--- src/components/Common/SkeletonTable/index.tsx
+++ src/components/Common/SkeletonTable/index.tsx
@@ -16,42 +16,45 @@ type Props = {
 }
 
 const SkeletonTable: FC<Props> = ({ rows = 2 }) => (
-  <Table>
-    <Head>
-      <HeaderRow>
-        <HeaderCell width="70">
-          <Skeleton width="30px" />
-        </HeaderCell>
-        <HeaderCell width="300">
-          <Skeleton width="40%" />
-        </HeaderCell>
-        <HeaderCell>
-          <Skeleton width="40%" />
-        </HeaderCell>
-        <HeaderCell>
-          <Skeleton width="40%" />
-        </HeaderCell>
-      </HeaderRow>
-    </Head>
-    <Body>
-      {Array.from(Array(rows).keys()).map((value) => (
-        <Row key={value}>
-          <Cell>
+  <>
+    <Skeleton noflex width="100px" height="32px" />
+    <Table>
+      <Head>
+        <HeaderRow>
+          <HeaderCell width="70">
             <Skeleton width="30px" />
-          </Cell>
-          <Cell>
-            <Skeleton width="20%" />
-          </Cell>
-          <Cell>
-            <Skeleton width="80%" />
-          </Cell>
-          <Cell>
-            <Skeleton width="50%" />
-          </Cell>
-        </Row>
-      ))}
-    </Body>
-  </Table>
+          </HeaderCell>
+          <HeaderCell width="300">
+            <Skeleton width="40%" />
+          </HeaderCell>
+          <HeaderCell>
+            <Skeleton width="40%" />
+          </HeaderCell>
+          <HeaderCell>
+            <Skeleton width="40%" />
+          </HeaderCell>
+        </HeaderRow>
+      </Head>
+      <Body>
+        {Array.from(Array(rows).keys()).map((value) => (
+          <Row key={value}>
+            <Cell>
+              <Skeleton width="30px" />
+            </Cell>
+            <Cell>
+              <Skeleton width="20%" />
+            </Cell>
+            <Cell>
+              <Skeleton width="80%" />
+            </Cell>
+            <Cell>
+              <Skeleton width="50%" />
+            </Cell>
+          </Row>
+        ))}
+      </Body>
+    </Table>
+  </>
 )
 
 export default memo(SkeletonTable)
