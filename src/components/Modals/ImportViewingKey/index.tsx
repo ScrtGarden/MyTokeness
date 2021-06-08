15e4769d3ba47924104c3ad7363ca03de4294c71
hieulai <>
2021-06-08 14:33:01 +1000

    ✨ add error handling

--- src/components/Modals/ImportViewingKey/index.tsx
+++ src/components/Modals/ImportViewingKey/index.tsx
@@ -1,5 +1,7 @@
-import { FC, memo, useState } from 'react'
+import { FC, memo, useEffect, useState } from 'react'
 
+import ButtonWithLoading from '../../Common/ButtonWithLoading'
+import MessageWithIcon from '../../Common/MessageWithIcon'
 import Icon from '../../Icons'
 import { Button } from '../../UI/Buttons'
 import { Field, Input, Label } from '../../UI/Forms'
@@ -8,12 +10,29 @@ import { Buttons, CloseButton, Content, Header, Title } from '../../UI/Modal'
 type Props = {
   toggle: () => void
   onClickPrimary: (value: string) => void
+  loading?: boolean
 }
 
 const ImportViewingKeyModal: FC<Props> = (props) => {
-  const { toggle, onClickPrimary } = props
+  const { toggle, onClickPrimary, loading } = props
 
   const [newKey, setNewKey] = useState('')
+  const [error, setError] = useState('')
+
+  const onClick = () => {
+    if (!newKey) {
+      setError('Please enter a valid key.')
+      return
+    }
+
+    onClickPrimary(newKey)
+  }
+
+  useEffect(() => {
+    if (error) {
+      setError('')
+    }
+  }, [newKey])
 
   return (
     <>
@@ -30,14 +49,20 @@ const ImportViewingKeyModal: FC<Props> = (props) => {
             autoFocus
             value={newKey}
             onChange={(e) => setNewKey(e.currentTarget.value)}
+            validation={error ? 'error' : undefined}
           />
+          {error && <MessageWithIcon validation="error" message={error} />}
         </Field>
       </Content>
       <Buttons>
         <Button onClick={toggle}>Cancel</Button>
-        <Button onClick={() => onClickPrimary(newKey)} isPrimary>
-          Import
-        </Button>
+        <ButtonWithLoading
+          text="Import"
+          onClick={onClick}
+          isPrimary
+          width={71}
+          loading={loading}
+        />
       </Buttons>
     </>
   )
