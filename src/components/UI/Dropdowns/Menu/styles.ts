5d3dcb655becbc16734e85945b6780fb8eb285dd
hieulai <>
2021-06-12 12:26:30 +1000

    💄 add isDanger prop

--- src/components/UI/Dropdowns/Menu/styles.ts
+++ src/components/UI/Dropdowns/Menu/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+interface ItemProps {
+  readonly isDanger?: boolean
+}
+
 const Container = styled.div`
   background: ${(props) => props.theme.fg};
   border: 2px solid ${(props) => props.theme.border.color};
@@ -7,12 +11,18 @@ const Container = styled.div`
   overflow: hidden;
 `
 
-const Item = styled.div`
+const Item = styled.div<ItemProps>`
   color: ${(props) => props.theme.font.colors.primary};
   cursor: pointer;
   font-size: ${(props) => props.theme.font.sizes.md};
   font-weight: ${(props) => props.theme.font.weights.semibold};
   padding: ${(props) => `${props.theme.space.sm} ${props.theme.space.md}`};
+
+  ${(props) =>
+    props.isDanger &&
+    `
+    color: ${props.theme.font.colors.warn};
+  `}
 
   :hover {
     background: ${(props) => props.theme.bg};
