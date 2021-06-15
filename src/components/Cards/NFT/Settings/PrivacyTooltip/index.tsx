da83ed4547da91560e1a0fc66400f9aed3c63a94
hieulai <>
2021-06-15 16:52:14 +1000

    💄 add label

--- src/components/Cards/NFT/Settings/PrivacyTooltip/index.tsx
+++ src/components/Cards/NFT/Settings/PrivacyTooltip/index.tsx
@@ -7,9 +7,10 @@ import { Container, Expiration } from './styles'
 
 type Props = {
   expiration: UIExpiration
+  label: string
 }
 
-const PrivacyTooltip: FC<Props> = ({ expiration }) => {
+const PrivacyTooltip: FC<Props> = ({ expiration, label }) => {
   const { type, date, blockheight } = expiration
   let value
   if (type === 'never') {
@@ -22,7 +23,7 @@ const PrivacyTooltip: FC<Props> = ({ expiration }) => {
 
   return (
     <Container>
-      Public ownership
+      {label}
       <Expiration>Exp: {value}</Expiration>
     </Container>
   )
