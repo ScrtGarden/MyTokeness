efd2f2919e5bc6e49366e1805b41bb08faeb8f18
hieulai <>
2021-06-17 21:40:47 +1000

    ✨ add asset route

---
+++ pages/nft/[contract:token].tsx
@@ -0,0 +1,10 @@
+import { getLayout as getSiteLayout } from '../../src/components/Layouts/SiteLayout'
+import NFTPage from '../../src/components/NFTPage'
+
+const getLayout = () => getSiteLayout(<NFTPage />, { hideSidebar: true })
+
+const Page = () => <NFTPage />
+
+Page.getLayout = getLayout
+
+export default Page
