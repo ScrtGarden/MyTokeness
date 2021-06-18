1cd61e929600ac91a49702ed986428920f639c23
hieulai <>
2021-06-18 13:57:01 +1000

    💄 add markdown

---
+++ src/components/UI/Markdown/index.ts
@@ -0,0 +1,25 @@
+import ReactMarkdown from 'react-markdown'
+import styled from 'styled-components'
+
+const Markdown = styled(ReactMarkdown)`
+  & > h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    color: ${(props) => props.theme.font.colors.primary};
+  }
+
+  p {
+    color: ${(props) => props.theme.font.colors.secondary};
+    font-size: ${(props) => props.theme.font.sizes.md};
+    line-height: ${(props) => props.theme.font.lineHeights.md};
+  }
+
+  ul {
+    color: ${(props) => props.theme.font.colors.secondary};
+    font-size: ${(props) => props.theme.font.sizes.md};
+  }
+`
+
+export { Markdown }
