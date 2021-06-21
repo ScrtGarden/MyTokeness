a4904cdf82084fe9a3711a9c7d74947fd21841c8
hieulai <>
2021-06-21 15:58:03 +1000

    ✨ hook that will create viewing key

---
+++ src/hooks/useMutationCreateNFTKey.ts
@@ -0,0 +1,22 @@
+import cryptoRandomString from 'crypto-random-string'
+import { useMutation } from 'react-query'
+import { ExecuteResult } from 'secretjs'
+
+import { HandleCreateViewingKey } from '../../interface/nft'
+import { MAX_GAS } from '../../utils/constants'
+import { executeContract } from '../../utils/contractInteractions'
+
+const useMutationCreateNFTKey = (contractAddress: string) =>
+  useMutation<ExecuteResult, Error>(() =>
+    executeContract<HandleCreateViewingKey>({
+      contractAddress,
+      maxGas: MAX_GAS.NFT.CREATE_VIEWING_KEY,
+      handleMsg: {
+        create_viewing_key: {
+          entropy: cryptoRandomString({ length: 40, type: 'base64' }),
+        },
+      },
+    })
+  )
+
+export default useMutationCreateNFTKey
