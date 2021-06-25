dcee4e8e9683393cebc5cbcd11d8420dadc8ddd6
hieulai <>
2021-06-25 15:36:34 +1000

    🔧 add maintenance mode

--- next.config.js
+++ next.config.js
@@ -2,4 +2,15 @@ module.exports = {
   images: {
     domains: ['ipfs.io'],
   },
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === '1'
+        ? {
+            source: '/((?!maintenance).*)',
+            destination: '/maintenance',
+            permanent: false,
+          }
+        : null,
+    ].filter(Boolean)
+  },
 }
