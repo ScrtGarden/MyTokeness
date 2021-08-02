f18547e08273e3a81c006ccda06a6f04604d2b47
hieulai <>
2021-08-03 09:53:50 +1000

    🔥 unused routes

--- pages/track/transfers.tsx
+++
@@ -1,8 +0,0 @@
-import { getLayout } from '../../src/components/Layouts/SiteLayout'
-import Transfers from '../../src/components/TrackPage/Transfers'
-
-const Page = () => <Transfers />
-
-Page.getLayout = getLayout
-
-export default Page
