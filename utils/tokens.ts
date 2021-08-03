b2efa0767aabbd7d06ab02a5c208d2f5d9458e8d
hieulai <>
2021-08-03 16:27:44 +1000

    🔧 add tokens that will be tracked

--- utils/tokens.ts
+++ utils/tokens.ts
@@ -11,6 +11,12 @@ interface Tokens {
   MAINNET: Record<string, Token>
 }
 
+interface TrackToken {
+  address: string
+  symbol: string
+  name: string
+}
+
 const TOKENS: Tokens = {
   TESTNET: {
     native: {
@@ -50,4 +56,51 @@ const TOKENS: Tokens = {
   MAINNET: {},
 }
 
-export default TOKENS
+const TRACK_TOKENS: { TESTNET: TrackToken[]; MAINNET: TrackToken[] } = {
+  TESTNET: [
+    {
+      address: 'secret1s7c6xp9wltthk5r6mmavql4xld5me3g37guhsx',
+      symbol: 'SSCRT',
+      name: 'Secret SCRT',
+    },
+    {
+      address: 'secret1e05fm04d5kulkgr4attqltnkmkfk7dhrnar89x',
+      symbol: 'MYDAI',
+      name: 'MYDAI',
+    },
+    {
+      address: 'secret1sxr4sq3ejp8eh0780wdxjjqd3tcyxgr2lyy5m6',
+      symbol: 'ONE',
+      name: 'ONE Coin',
+    },
+  ],
+  MAINNET: [
+    {
+      address: 'secret1k0jntykt7e4g3y88ltc60czgjuqdy4c9e8fzek',
+      symbol: 'SSCRT',
+      name: 'Secret SCRT',
+    },
+    {
+      address: 'secret15l9cqgz5uezgydrglaak5ahfac69kmx2qpd6xt',
+      name: 'Secret Finance',
+      symbol: 'SEFI',
+    },
+    {
+      address: 'secret1wuzzjsdhthpvuyeeyhfq2ftsn3mvwf9rxy6ykw',
+      name: 'Ethereum',
+      symbol: 'ETH',
+    },
+    {
+      address: 'secret18wpjn83dayu4meu6wnn29khfkwdxs7kyrz9c8f',
+      name: 'Tether',
+      symbol: 'USDT',
+    },
+    {
+      address: 'secret1vnjck36ld45apf8u4fedxd5zy7f5l92y3w5qwq',
+      name: 'Dai Stablecoin',
+      symbol: 'DAI',
+    },
+  ],
+}
+
+export { TOKENS as default, TRACK_TOKENS }
