07b1c279f0b5792180d41cce413f56f9ea7a5a66
hieulai <>
2021-08-03 14:14:19 +1000

    💄 update AddressWithCopy v2

--- src/components/TrackPage/Table/TransferCell/index.tsx
+++ src/components/TrackPage/Table/TransferCell/index.tsx
@@ -2,7 +2,7 @@ import { FC, memo, useMemo } from 'react'
 import { Coin } from 'secretjs/types/types'
 
 import toBiggestDenomination from '../../../../../utils/toBiggestDenomination'
-import AddressWithCopy from '../../../Common/AddressWithCopy'
+import AddressWithCopy from '../../../Common/AddressWithCopyV2'
 import { Amount, Container, StyledIcon, Wrapper } from './styles'
 
 type Props = {
@@ -30,7 +30,10 @@ const TransferCell: FC<Props> = ({
     <Container>
       <Wrapper>
         <StyledIcon name="wallet-duo" height={25} width={25} />
-        <AddressWithCopy address={isDeposit ? from : receiver} />
+        <AddressWithCopy
+          address={isDeposit ? from : receiver}
+          placement="right"
+        />
       </Wrapper>
       <Amount deposit={isDeposit ? 'true' : 'false'}>
         {isDeposit ? '+' : '-'}
