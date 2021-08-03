807b5c72534a2dc34fea9d1ef6747e01f6e6ae23
hieulai <>
2021-08-03 14:13:55 +1000

    🔥 remove unused code

--- src/components/TrackPage/styles.ts
+++ src/components/TrackPage/styles.ts
@@ -9,16 +9,8 @@ const Container = styled.div`
   row-gap: ${(props) => props.theme.space.md};
 `
 
-const Content = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  row-gap: ${(props) => props.theme.space.xl};
-  width: 100%;
-`
-
 const StyledEmptyList = styled(EmptyList)`
   width: 100%;
 `
 
-export { Content, Container, StyledEmptyList }
+export { Container, StyledEmptyList }
