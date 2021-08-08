5b3c10c8ebddc3ded279f85fae1655b695bd68fb
hieulai <>
2021-08-08 15:22:52 +1000

    ✨ functions that parse data for ui

--- src/components/TrackPage/Table/lib.ts
+++ src/components/TrackPage/Table/lib.ts
@@ -1,11 +1,14 @@
 import { TxAction } from '../../../../interface/snip20'
+import { DAPPS } from '../../../../utils/dapps'
+import truncateAddress from '../../../../utils/truncateAddress'
 import { IconName } from '../../Icons'
 
 interface ParseTransactionResult {
   type: string
   icon: IconName
   isDeposit: boolean | null
-  address: string
+  name: string
+  description?: string
   from?: string
   sender?: string
   recipient?: string
@@ -14,19 +17,24 @@ interface ParseTransactionResult {
   minter?: string
 }
 
+interface ParseTransferResult {
+  icon: IconName
+  isDeposit: boolean | null
+  name: string
+  description?: string
+}
+
 const parseTransaction = (
   action: TxAction,
   walletAddress?: string
 ): ParseTransactionResult => {
   if (action.transfer) {
     const { from, recipient } = action.transfer
-    const isOnlySender = from !== walletAddress && recipient !== walletAddress
-    const isDeposit = recipient === walletAddress
+    const parsedData = parseTransfer(from, recipient, walletAddress)
+
     return {
       type: 'transfer',
-      icon: 'wallet-duo',
-      isDeposit: isOnlySender ? null : isDeposit,
-      address: isDeposit ? from : recipient,
+      ...parsedData,
       ...action.transfer,
     }
   } else if (action.mint) {
@@ -34,7 +42,7 @@ const parseTransaction = (
       type: 'mint',
       icon: 'hand-holding-usd',
       isDeposit: true,
-      address: action.mint.recipient,
+      name: truncateAddress(action.mint.recipient),
       ...action.mint,
     }
   } else if (action.burn) {
@@ -42,23 +50,23 @@ const parseTransaction = (
       type: 'burn',
       icon: 'fire-duo',
       isDeposit: false,
-      address: action.burn.owner,
+      name: truncateAddress(action.burn.owner),
       ...action.burn,
     }
   } else if (action.deposit) {
     return {
       type: 'deposit',
-      icon: 'dizzy',
+      icon: 'piggy-bank-duo',
       isDeposit: true,
-      address: action.deposit.recipient,
+      name: truncateAddress(action.deposit.recipient),
       ...action.deposit,
     }
   } else if (action.redeem) {
     return {
       type: 'redeem',
-      icon: 'dizzy',
+      icon: 'ticket-duo',
       isDeposit: false,
-      address: action.redeem.recipient || action.redeem.owner,
+      name: truncateAddress(action.redeem.recipient || action.redeem.owner),
       ...action.redeem,
     }
   }
@@ -66,8 +74,42 @@ const parseTransaction = (
     type: '',
     icon: 'dizzy',
     isDeposit: false,
-    address: '',
+    name: '',
   }
 }
 
-export { parseTransaction }
+const parseTransfer = (
+  from: string,
+  receiver: string,
+  walletAddress?: string
+): ParseTransferResult => {
+  const isOnlySender = from !== walletAddress && receiver !== walletAddress
+  const isDeposit = isOnlySender ? null : receiver === walletAddress
+
+  let icon: IconName = 'wallet-duo'
+  let name = ''
+  let description = ''
+
+  if (isDeposit === null) {
+    icon = 'exchange-duo'
+    name = `${truncateAddress(from, 7, 4)} → ${truncateAddress(receiver, 7, 4)}`
+  } else {
+    const dapp = DAPPS[isDeposit ? from : receiver]
+    if (dapp) {
+      icon = dapp.icon
+      name = dapp.name
+      description = dapp.description
+    } else {
+      name = truncateAddress(isDeposit ? from : receiver)
+    }
+  }
+
+  return {
+    isDeposit,
+    icon,
+    name,
+    description,
+  }
+}
+
+export { parseTransaction, parseTransfer }
